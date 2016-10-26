22e0f7f29c9693529f24ae5cf068ece6b0b8baf1
Jose Riguera <jriguera@gmail.com>
2016-10-26 18:03:49 +0200

    move __meta__ tags to __init__

--- cfconfigurator/__init__.py
+++ cfconfigurator/__init__.py
@@ -3,3 +3,10 @@
 """
 python-cfconfigurator
 """
+
+__program__ = "cfconfigurator"
+__version__ = "0.2.0"
+__author__ = "Jose Riguera"
+__year__ = "2016"
+__email__ = "<jose.riguera@springer.com>"
+__license__ = "MIT"

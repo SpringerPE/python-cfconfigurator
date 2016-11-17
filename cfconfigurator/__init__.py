c5416b3e73a1095db2baa53403737b7bc57adba8
Jose Riguera <jriguera@gmail.com>
2016-11-17 15:19:49 +0100

    v0.2.1

--- cfconfigurator/__init__.py
+++ cfconfigurator/__init__.py
@@ -5,7 +5,7 @@ python-cfconfigurator
 """
 
 __program__ = "cfconfigurator"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Jose Riguera"
 __year__ = "2016"
 __email__ = "<jose.riguera@springer.com>"

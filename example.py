b32cf884c9acd5e6bf004afeba26ec8f5a8274c0
Jose Riguera <jriguera@gmail.com>
2017-01-23 18:56:31 +0100

    pagination example

--- example.py
+++ example.py
@@ -5,10 +5,17 @@ from cfconfigurator.cf import CF
 
 api_url = "https://api.test.cf.springer-sbm.com"
 admin_user = "admin"
-admin_password = "admin"
+admin_password = "password"
 
 cf = CF(api_url)
 cf.login(admin_user, admin_password)
 
 org = cf.search_org("pivotal")
 print(org)
+
+services = cf.request('GET', "/v2/services", {"results-per-page": 1})
+print(services)
+
+services = cf.request('GET', "https://api.test.cf.springer-sbm.com/v2/services", {"results-per-page": 1})
+print(services)
+

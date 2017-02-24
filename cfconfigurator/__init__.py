7c7ad5e8367bc54a6fdd72184358215678f829c5
Jose Riguera <jriguera@gmail.com>
2017-02-24 17:33:22 +0100

    version 0.2.3

--- cfconfigurator/__init__.py
+++ cfconfigurator/__init__.py
@@ -5,8 +5,8 @@ python-cfconfigurator
 """
 
 __program__ = "cfconfigurator"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "Jose Riguera"
-__year__ = "2016"
+__year__ = "2017"
 __email__ = "<jose.riguera@springer.com>"
 __license__ = "MIT"
